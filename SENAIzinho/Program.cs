a72c70d3bcbc2ac4db2d8f9c9783dedf862bdc14
lucasryg <lucasryg@gmail.com>
2019-10-10 17:24:16 -0300

    ex

--- SENAIzinho/Program.cs
+++ SENAIzinho/Program.cs
@@ -1,11 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SENAIzinho {
     class Program {
+            
         static void Main (string[] args) {
-            List<Sala> AlocarAluno = new List<Sala> ();
+
+            int limiteAlunos = 10;
+            int limiteSalas = 10;
+            Aluno[] alunos = new Aluno[100];
+            Sala[] salas = new Sala[10];
+            int alunosCadastrados = 0;
+            int salasCadastradas = 0;
+            // bool querSair;
+
 
         }
+        public void CadastrarAluno(Aluno[] alunos, int totalAluno, int alunosCadastrados){
+            if(totalAluno < alunosCadastrados)
+            {
+                System.Console.WriteLine("Alunos Cadastrados: ");
+                System.Console.WriteLine();
+                System.Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+                System.Console.WriteLine("Dt Nascimento: ");
+                DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+                Aluno novoAluno = new Aluno();
+                novoAluno.Nome = nome;
+                novoAluno.DataNascimento = dtNascimento;
+
+                int index = 0;
+                foreach(Aluno aluno in alunos)
+                {
+                    if(alunos == null)
+                    {
+                        alunos[index] = novoAluno;
+                        break;
+                    }
+                    index++;
+                }
+                alunosCadastrados++;
+            }
+        }
+        public void CadastrarSala(){}
+        public void AlocarAluno(){}
+        public void RemoverAluno(){}
+        public void VerificarSalas(){}
+        public void VerificarAlunos(){}
+
+
     }
 }
--- SENAIzinho/Sala.cs
+++ SENAIzinho/Sala.cs
@@ -1,53 +1,113 @@
+using System;
+
 namespace SENAIzinho {
     public class Sala {
-        int capacidadeAtual { get; set; }
-        int capacidadeTotal { get; set; }
-        int numeroSala { get; set; }
-        string[] Alunos { get; set; }
+        int capacidadeAtual;
+        int capacidadeTotal;
+        int numeroSala;
+        string[] Alunos;
 
-        //Cosntrutores
+        //Construtores
         public Sala (int numeroSala, int capacidadeTotal) {
             this.numeroSala = numeroSala;
             this.capacidadeTotal = capacidadeTotal;
+            this.capacidadeAtual = this.capacidadeTotal;
+            this.Alunos = new string[capacidadeTotal];
         }
-        public string AlocarAluno (string Alunos) {
-            this.Alunos = Alunos;
-        }
-        public string RemoverAluno (string Alunos) {
-            this.Alunos = Alunos;
-        }
-
         //Metodos
 
-        public string AlocarAluno(string nomeAluno){
-            System.Console.WriteLine("Nome do Aluno a ser cadastrado na sala: " );
-            nomeAluno = Console.ReadLine();
+        public string AlocarAluno(string NomeAluno)
+        {
+            int index = 0;
 
-            int capacidadeAtual = 0;
-
-            Alunos[capacidadeAtual] = nomeAluno;
-
-            if(capacidadeAtual > 10){
-                System.Console.WriteLine("Nao há vagas nessa sala!");
-            } else{
-                System.Console.WriteLine("Nome do aluno cadastrado com sucesso!");
-                capacidadeAtual++;
+            if(this.capacidadeAtual > 0)
+            {
+                foreach(string aluno in this.Alunos)
+                {
+                    if(aluno == "")
+                    {
+                        this.Alunos[index] = NomeAluno;
+                        break;
+                    }
+                    index++;
+                }
+                this.capacidadeAtual--;
+                return "ok";
+            }else
+            {
+                return "LOTADO";    
             }
-
         }
 
-        public string RemoverAluno(string removAluno){
-            System.Console.WriteLine("Nome do aluno que você deseja retirar da sala: ");
-            removAluno = Console.ReadLine();
-            for(Alunos[0];Aluno[99];Alunos++)
+        public string RemoverAluno(string nomeAluno)
+        {
+            int index = 0;
+
+            if(this.capacidadeAtual == this.capacidadeTotal){
+                return "SALAVAZIA";
+            }
+
+            foreach(string aluno in this.Alunos)
+            {
+                if(nomeAluno == aluno)
+                {
+                    this.Alunos[index] = "";
+                    return "OK";
+                }
+                index++;
+            }
+            return "NAOENCONTRADO";
+        }
+
+        public String MostrarAlunos()
+        {
+            string listaAlunos = "";
+
+            foreach(string aluno in this.Alunos)
+            {
+                if(aluno != "")
+                {
+                    listaAlunos = listaAlunos + aluno + " ";
+                }
+            }
+            return listaAlunos;
+        }
+
+        #region Meus codigos
+        // public string AlocarAluno (string nomeAluno) {
             
-            foreach(string nomeAluno in Alunos){
-            if(nomeAluno == removAluno){
-                System.Console.WriteLine("Aluno foi removido com sucesso!");
-            } else{
-                System.Console.WriteLine("Nao encontramos o aluno no sistema");
-            }
-            }
+        //     System.Console.WriteLine ("Nome do Aluno a ser cadastrado na sala: ");
+        //     nomeAluno = Console.ReadLine();
+            
+        //     int capacidadeAtual = 0;
+
+        //     Alunos[capacidadeAtual] = nomeAluno;
+
+        //     if (capacidadeAtual > 10) {
+        //         System.Console.WriteLine ("Nao há vagas nessa sala!");
+        //     } else {
+        //         System.Console.WriteLine ("Nome do aluno cadastrado com sucesso!");
+        //         capacidadeAtual = capacidadeAtual + 1 ;
+        //     }
+            
+        // }
+        
+
+        
+
+        // public string RemoverAluno (string removAluno) {
+        //     System.Console.WriteLine ("Nome do aluno que você deseja retirar da sala: ");
+        //     removAluno = Console.ReadLine ();
+            
+        //         foreach (string nomeAluno in Alunos) {
+        //             if (nomeAluno == removAluno) {
+        //                 System.Console.WriteLine ("Aluno foi removido com sucesso!");
+        //             } else {
+        //                 System.Console.WriteLine ("Nao encontramos o aluno no sistema");
+        //             }
+        //         }
+        //     }
+        #endregion 
         }
-    }
 }
+
